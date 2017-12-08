205c91194ce0ca9da45b15c5e49835cc335ab368
KIU Shueng Chuan <nixchuan@gmail.com>
2017-12-08 22:10:57 +0800

    create CubicSpline class
    aim to be compatible with scipy.interpolate.CubicSpline

--- mkl_df.py
+++ mkl_df.py
@@ -7,10 +7,10 @@ ffi = cffi.FFI()
 ffi.cdef("""
 typedef void* DFTaskPtr;
 
-int dfsNewTask1D(DFTaskPtr *, int, const float[], int, int, const float[], int);
-int dfsEditPPSpline1D(DFTaskPtr, int, int, int, const float[], int, const float[], const float[], int);
-int dfsConstruct1D(DFTaskPtr, int, int);
-int dfsInterpolate1D(DFTaskPtr, int, int, int, const float[], int, int, const int[], const float[], float[], int, int[]);
+int dfdNewTask1D(DFTaskPtr *, int, const double[], int, int, const double[], int);
+int dfdEditPPSpline1D(DFTaskPtr, int, int, int, const double[], int, const double[], const double[], int);
+int dfdConstruct1D(DFTaskPtr, int, int);
+int dfdInterpolate1D(DFTaskPtr, int, int, int, const double[], int, int, const int[], const double[], double[], int, int[]);
 int dfDeleteTask(DFTaskPtr *);
 
 #define DF_STATUS_OK 0
@@ -27,6 +27,7 @@ int dfDeleteTask(DFTaskPtr *);
 #define DF_NO_BC 0
 #define DF_BC_NOT_A_KNOT 1
 #define DF_BC_FREE_END 2
+#define DF_BC_PERIODIC 64
 
 #define DF_NO_IC 0
 
@@ -92,34 +93,34 @@ def raise_if_error(rc):
         raise DfError(error_message(rc))
 
 class DfTask:
-    def __init__(self, x, y):
+    def __init__(self, x, y, bc_type=lib.DF_BC_FREE_END):
         self.task = ffi.new("DFTaskPtr*")
-        rc = lib.dfsNewTask1D(self.task, x.size, ffi.cast("float*", x.ctypes.data), lib.DF_NON_UNIFORM_PARTITION, 1, ffi.cast("float*", y.ctypes.data), lib.DF_NO_HINT)
+        rc = lib.dfdNewTask1D(self.task, x.size, ffi.cast("double*", x.ctypes.data), lib.DF_NON_UNIFORM_PARTITION, 1, ffi.cast("double*", y.ctypes.data), lib.DF_NO_HINT)
         raise_if_error(rc)
         
         s_order = lib.DF_PP_CUBIC
         s_type = lib.DF_PP_NATURAL
-        bc_type = lib.DF_BC_FREE_END
         bc = ffi.NULL
         ic_type = lib.DF_NO_IC
         ic = ffi.NULL
-        self.scoeff = np.empty((x.size-1)*s_order, dtype=np.float32)
+        self.scoeff = np.empty((x.size-1)*s_order, dtype=x.dtype)
         scoeffhint = lib.DF_NO_HINT
-        rc = lib.dfsEditPPSpline1D(self.task[0], s_order, s_type, bc_type, bc, ic_type, ic, ffi.cast("float*", self.scoeff.ctypes.data), scoeffhint)
+        rc = lib.dfdEditPPSpline1D(self.task[0], s_order, s_type, bc_type, bc, ic_type, ic, ffi.cast("double*", self.scoeff.ctypes.data), scoeffhint)
         raise_if_error(rc)
-        rc = lib.dfsConstruct1D(self.task[0], lib.DF_PP_SPLINE, lib.DF_METHOD_STD)
+        rc = lib.dfdConstruct1D(self.task[0], lib.DF_PP_SPLINE, lib.DF_METHOD_STD)
         raise_if_error(rc)
 
     def interpolate(self, site, ndorder=1):
         sitehint = lib.DF_NON_UNIFORM_PARTITION
-        dorder = ffi.new("int[]", [1]*ndorder)
+        dorder = ffi.new("int[]", ndorder)
+        dorder[ndorder-1] = 1
         # there is an error with Intel's example, datahint is a float array, just happens that DF_NO_APRIORI_INFO=0
         # datahint = lib.DF_NO_APRIORI_INFO
         datahint = ffi.NULL
-        r = np.empty((site.size, ndorder), dtype=site.dtype)
+        r = np.empty_like(site)
         rhint = lib.DF_MATRIX_STORAGE_ROWS
         cell = ffi.NULL
-        rc = lib.dfsInterpolate1D(self.task[0], lib.DF_INTERP, lib.DF_METHOD_PP, site.size, ffi.cast("float*", site.ctypes.data), sitehint, ndorder, dorder, datahint, ffi.cast("float*", r.ctypes.data), rhint, cell)
+        rc = lib.dfdInterpolate1D(self.task[0], lib.DF_INTERP, lib.DF_METHOD_PP, site.size, ffi.cast("double*", site.ctypes.data), sitehint, ndorder, dorder, datahint, ffi.cast("double*", r.ctypes.data), rhint, cell)
         raise_if_error(rc)
         return r
     
@@ -127,22 +128,38 @@ class DfTask:
         rc = lib.dfDeleteTask(self.task)
         raise_if_error(rc)
 
+class CubicSpline:
+    def __init__(self, x, y, bc_type='not-a-knot'):
+        if bc_type=='not-a-knot':
+            bc_type=lib.DF_BC_NOT_A_KNOT
+        elif bc_type=='periodic':
+            bc_type=lib.DF_BC_PERIODIC
+        elif bc_type=='clamped':
+            pass
+        elif bc_type=='natural':
+            bc_type=lib.DF_BC_FREE_END
+            
+        self.dftask = DfTask(x, y, bc_type)
+    
+    def __call__(self, x, nu=0):
+        return self.dftask.interpolate(x, ndorder=nu+1)
+
 if __name__=='__main__':
     import matplotlib.pyplot as plt
+    # from scipy.interpolate import CubicSpline
     
     # 100 Hz sampling
-    x0 = 2*np.pi*2*np.linspace(0, 1, 101, dtype=np.float32)
+    x0 = 2*np.pi*2*np.linspace(0, 1, 101)
     y0 = np.cos(x0)
     dy0 = -np.sin(x0)
     
     # 10 Hz sampling
     x1 = x0[::10].copy()
     y1 = y0[::10].copy()
-    task = DfTask(x1, y1)
+    cs = CubicSpline(x1, y1, bc_type='natural')
+    y = cs(x0)
+    dy = cs(x0, 1)
 
-    r = task.interpolate(x0, ndorder=2)
-    y, dy = r[:,0], r[:,1]
-    
     plt.plot(y0)
     plt.plot(dy0)
     plt.plot(y, '.')
